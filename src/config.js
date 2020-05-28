917e1b665c0efd7b84e1321a0471ab772eb3cefc
Nathaly Cuya <nathalycuya@utexas.edu>
2020-05-28 01:01:50 -0500

    update

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
-  NODE_ENV: process.env.NODE_ENV || 'production',
+  NODE_ENV: process.env.NODE_ENV || 'development',
   DATABASE_URL: process.env.DATABASE_URL || 'postgresql://dunder_mifflin@localhost/plant-api',  
   TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://dunder_mifflin@localhost/plant-test',
 }
